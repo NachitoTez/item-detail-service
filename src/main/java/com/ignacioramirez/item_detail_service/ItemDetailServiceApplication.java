ecfd1bcf41a30f18c7621ebc4526d3dca251c008
NachitoTez <ignacio.ramirezguembe@gmail.com>
2025-10-05 20:21:54 -0300

    Springboot configuration

---
+++ src/main/java/com/ignacioramirez/item_detail_service/ItemDetailServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ignacioramirez.item_detail_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemDetailServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemDetailServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ignacioramirez/item_detail_service/ItemDetailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ignacioramirez.item_detail_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemDetailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
