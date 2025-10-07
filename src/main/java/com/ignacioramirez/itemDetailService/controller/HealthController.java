c1787816a95e0ad4bf14895819f9a8419750265d
NachitoTez <ignacio.ramirezguembe@gmail.com>
2025-10-06 22:23:09 -0300

    Healthcheck

---
+++ src/main/java/com/ignacioramirez/itemDetailService/controller/HealthController.java
@@ -0,0 +1,18 @@
+package com.ignacioramirez.itemDetailService.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+
+    @GetMapping
+    public ResponseEntity<Map<String, Object>> health() {
+        return ResponseEntity.ok(Map.of(
+                "status", "UP"
+        ));
+    }
+}
